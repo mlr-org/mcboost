959341d3a0fa25012ff5acdd246e0080d67cc31e
pfistfl <pfistererf@googlemail.com>
2022-07-26 21:39:25 +0200

    remove testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,6 @@ if (requireNamespace("testthat", quietly = TRUE)) {
   library(testthat)
   library(mlr3)
   library(mcboost)
-  library(mlr3proba)
   library(mlr3pipelines)
 
   test_check("mcboost")
